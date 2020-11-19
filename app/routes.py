6f89a54a6c23ebc0f8d6b300752aa4c333ef143d
timchen0618 <b05901101@ntu.edu.tw>
2020-11-19 17:08:56 +0800

    finish example

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,47 @@
 from app import app
 from flask import render_template, flash, request, redirect, url_for
 from app.forms import LoginForm
-from app.control import ctrl
+from src.test import Generator
+# from app.control import ctrl
+import json
 
+TABLE = json.load(open('data/new_table.json'))
+
+NAMES = ['Dell Laptop Latitude E6440', 
+         'Dell Vostro 3458', 
+         'Apple iMac 27', 
+         'HP Z820 Workstation', 
+         'iBUYPOWER Gamer Supreme', 
+         'Toshiba Tecra C50-B1503', 
+         'Asus Z91', 
+         'Lenovo T530'
+         ]
+generator = Generator(checkpoint='src/checkpoint.pth')
+print('finish loading generator')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
-    pattern_string = ""
+    product = request.args.get('product')
+    table = {}
+    description = ""
+    print('damn')
+    try:
+        print('pro', product)
+        num = int(product)
+        table = TABLE[num-1]
+        name = NAMES[num-1]
 
-    pattern = request.args.get('pattern')
-    suggestions = []
-    if pattern:
-        sug = ctrl.intent_pattern[pattern]
-        for s in sug:
-            if s.find('name') == -1:
-                suggestions.append(s)
+    except:
+        print('ggg')
+        return render_template('base.html', form=form, table=table, description=description, product_list=NAMES)
 
+    print('table')
+    description = generator.test(table) 
+    table = list(table.items())
     if form.validate_on_submit():
         print('fff')
-        command = form.inputtext.data
-        flash('已輸入指令: {}'.format(command))
-        cmd_string, pattern_string = ctrl.control(command)
-        zh_string = ""
-        for k in CMD_LIST:
-            if k[0] == cmd_string:
-                zh_string = k[1]
-        flash('Pattern: [{} {}] {}'.format(cmd_string, zh_string, pattern_string))
-        # return redirect(url_for(cmd_string))
-        print(cmd_string)
-        return render_template('index.html', title='Intention Demo', form=form, cmd=cmd_string, cmd_list=CMD_LIST, suggestions=suggestions)
-    return render_template('index.html', title='Intention Demo', form=form, cmd="", cmd_list=CMD_LIST, suggestions=suggestions)
+        print(description)
+               
+        return render_template('base.html', form=form, table=table, description=description, product_list=NAMES)
+    return render_template('base.html', form=form, table=table, description=description, product_list=NAMES)
---
+++ src/test.py
@@ -0,0 +1,84 @@
+import time
+import os
+
+from src.utils import make_save_dir, read_json
+from src.models import GenerationModel, TemplateHandler
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from transformers import T5Model, T5ForConditionalGeneration, T5Tokenizer
+import logging
+
+class Generator(object):
+    """docstring for Generator"""
+    def __init__(self, condition_generation=True, checkpoint='checkpoint.pth'):
+        super(Generator, self).__init__()
+        print('preparing dictionary...')
+        # prepare dictionary, tokenizer
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-base', padding_side='right')
+        self.word2id = self.tokenizer.get_vocab()
+        self.bos = self.word2id[self.tokenizer.pad_token]
+
+        self.src_max_len = 512
+        self.tgt_max_len = 200
+        print('preparing model...')
+        # prepare model
+        self._saved_checkpoint = checkpoint
+        self.prepare_model(condition_generation=condition_generation)
+        self._condition_generation = condition_generation
+        
+
+    def prepare_model(self, condition_generation=False):
+        if condition_generation:
+            self.model = T5ForConditionalGeneration.from_pretrained('t5-base')
+        else:
+            t5_model = T5Model.from_pretrained('t5-base')
+            self.model = GenerationModel(t5_model)
+        self.load_checkpoint()
+
+    def _generate_one_step(self, input_ids):
+        if self._condition_generation:
+            outputs = self.model.generate(input_ids, max_length=self.tgt_max_len)
+        else:
+            outputs = self.model.generate(input_ids, max_length=self.tgt_max_len, bos_token=self.bos)
+        return outputs
+
+    def load_checkpoint(self):
+        print('loading checkpoint')
+        state_dict = torch.load(self._saved_checkpoint, map_location=torch.device('cpu'))['state_dict']
+        self.model.load_state_dict(state_dict)
+
+    def tokenize(self, x):
+        return self.tokenizer.encode(x, max_length=self.src_max_len,
+                                        truncation=True,
+                                        return_tensors='pt',
+                                        pad_to_max_length=True
+                                    )
+
+    def id2sent(self, indices):
+        return ' '.join([self.index2word[int(w)] for w in indices])
+
+    @torch.no_grad()
+    def test(self, table):
+        print('='*30)
+        print('========== Testing ==========')
+        print('='*30)
+
+        self.model.eval()
+
+        input_str = ''.join(['<'+k+'> '+v+' </'+k+'>\n' for k, v in table.items()])
+        input_ids = self.tokenize(input_str).squeeze(0)
+        print('input_ids', input_ids.size())
+        outputs = self._generate_one_step(input_ids.unsqueeze(0))
+        print('outputs', outputs.size())
+
+        l = self.tokenizer.decode(outputs.squeeze(0).long().tolist())
+        return l
+
+
+
+
+
