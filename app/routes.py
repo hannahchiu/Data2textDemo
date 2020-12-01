df2eee1f5bb4fbadeec9b1427cc499017958d91b
timchen0618 <b05901101@ntu.edu.tw>
2020-12-01 16:26:12 +0800

    data2text-zh

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, flash, request, redirect, url_for
 from app.forms import LoginForm
-from src.test import Generator, all_slots
+from src.test import Generator, all_slots, GeneratorZH, all_slots_zh
 # from app.control import ctrl
 import json
 
@@ -16,10 +16,22 @@ NAMES = ['Dell Laptop Latitude E6440',
          'Asus Z91', 
          'Lenovo T530'
          ]
-generator = Generator(checkpoint='src/checkpoint.pth')
-print('finish loading generator')
+# generator = Generator(checkpoint='src/checkpoint.pth')
+# print('finish loading generator')
 
+TABLE_ZH = json.load(open('data/zh_data.json'))
 
+NAMES_ZH = ['【STEIFF】熊頭童裝 長袖T恤',
+'【Footer】輕壓力單色足弓襪',
+'【ILEY 伊蕾】100%縲縈碎花長版洋裝',
+'【初色】休閒條紋拼接上衣-藏藍色',
+'【RIVER WOODS】簡約質感V領長袖針織衫',
+'【MON’S】浪漫花版修身洋裝',
+'【XLARGE】XLARGE S/S TEE REFLECTOR DRAWING OG短袖T恤',
+'【Jessica Red】氣質素色圓領綁帶造型上衣',
+'【SNOOPY 史努比】史努比穿毛衣寬版大學T'
+]
+"""
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
@@ -55,7 +67,8 @@ def index():
 
             if table:
                 print('table', table)
-                description = generator.test(table) 
+                # description = generator.test(table) 
+                description = "sleghsefs"
                 print(description)
         tuples = list(table.items())
         return render_template('index.html', form=form, table=tuples, description=description, product_list=NAMES)
@@ -66,5 +79,60 @@ def index():
 @app.route('/attr', methods=['GET', 'POST'])
 def attr():
     return render_template('attr.html', slots=all_slots)
+
+"""
+
+
+generator_zh = GeneratorZH(checkpoint='src/checkpoint.zh.pth')
+print('finish loading generator')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = LoginForm()
+    product = request.args.get('product')
+    table = {}
+    description = ""
+    print('damn')
+    # if form.validate_on_submit():
+    #     print('fff')
+    #     return render_template('index.html', form=form, table=tuples, description=description, product_list=NAMES)
+
+    try:
+        print('pro', product)
+        num = int(product)
+        table = TABLE_ZH[num-1]
+        name = NAMES_ZH[num-1]
+        global_table = table
+    except:
+        if product == 'submit':
+            print('ggg')
+            ### handle table here ###
+            
+            # print(request.form)
+            num_form = (len(request.form))//2
+            table = {}
+            form_table = request.form.to_dict(flat=False)
+            # print(form_table)
+            for i in range(num_form):
+                # print(form_table['attr-%d'%i], form_table['value-%d'%i])
+                if not request.form['attr-%d'%i]:
+                    continue
+                table[request.form['attr-%d'%i]] = request.form['value-%d'%i]
+
+            if table:
+                print('table', table)
+                description = generator_zh.test(table) 
+                # description = "sleghsefs"
+                print(description)
+        tuples = list(table.items())
+        return render_template('index.html', form=form, table=tuples, description=description, product_list=NAMES_ZH)
+
+    tuples = list(table.items())
+    return render_template('index.html', form=form, table=tuples, description=description, product_list=NAMES_ZH)
+
+@app.route('/attr', methods=['GET', 'POST'])
+def attr():
+    return render_template('attr.html', slots=all_slots_zh)
+
 
 
--- src/test.py
+++ src/test.py
@@ -10,7 +10,11 @@ import torch.nn as nn
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from transformers import T5Model, T5ForConditionalGeneration, T5Tokenizer
+from transformers import MT5Model, MT5ForConditionalGeneration
 import logging
+import opencc
+
+converter = opencc.OpenCC('s2tw.json')
 
 all_slots = [
 'Brand Name', 
@@ -121,7 +125,7 @@ class Generator(object):
         return self.tokenizer.encode(x, max_length=self.src_max_len,
                                         truncation=True,
                                         return_tensors='pt',
-                                        pad_to_max_length=True
+                                        padding='max_length'
                                     )
 
     def id2sent(self, indices):
@@ -144,7 +148,74 @@ class Generator(object):
         l = self.tokenizer.decode(outputs.squeeze(0).long().tolist())
         return l
 
+all_slots_zh = ['效能', '荷重', '功效', '對象與族群', '圖案', '顏色', '成份', '品牌定位', '材質', '認證', '容量', '香味', 'description', '品牌名稱', '功能', '組裝方式', '形狀', '罩杯', '適用於', '類型', '款式', '重量', '包裝組合', '版型', '尺寸', '產地', '領圍', '時間']
 
+class GeneratorZH(object):
+    """docstring for Generator"""
+    def __init__(self, condition_generation=True, checkpoint='checkpoint.zh.pth'):
+        super(GeneratorZH, self).__init__()
+        print('preparing dictionary...')
+        # prepare dictionary, tokenizer
+        self.tokenizer = T5Tokenizer.from_pretrained('google/mt5-base', padding_side='right')
+        self.word2id = self.tokenizer.get_vocab()
+        self.bos = self.word2id[self.tokenizer.pad_token]
 
+        self.src_max_len = 300
+        self.tgt_max_len = 100
+        print('preparing model...')
+        # prepare model
+        self._saved_checkpoint = checkpoint
+        self.prepare_model(condition_generation=condition_generation)
+        self._condition_generation = condition_generation
+        
+
+    def prepare_model(self, condition_generation=False):
+        if condition_generation:
+            self.model = MT5ForConditionalGeneration.from_pretrained('google/mt5-base')
+        else:
+            t5_model = MT5Model.from_pretrained('google/mt5-base')
+            self.model = GenerationModel(t5_model)
+        self.load_checkpoint()
+
+    def _generate_one_step(self, input_ids):
+        if self._condition_generation:
+            outputs = self.model.generate(input_ids, max_length=self.tgt_max_len, no_repeat_ngram_size=3, num_beams=4)
+        else:
+            outputs = self.model.generate(input_ids, max_length=self.tgt_max_len, bos_token=self.bos)
+        return outputs
+
+    def load_checkpoint(self):
+        print('loading checkpoint')
+        state_dict = torch.load(self._saved_checkpoint, map_location=torch.device('cpu'))['state_dict']
+        self.model.load_state_dict(state_dict)
+
+    def tokenize(self, x):
+        return self.tokenizer.encode(x, max_length=self.src_max_len,
+                                        truncation=True,
+                                        return_tensors='pt',
+                                        padding='max_length'
+                                    )
+
+    def id2sent(self, indices):
+        return ' '.join([self.index2word[int(w)] for w in indices])
+
+    @torch.no_grad()
+    def test(self, table):
+        print('='*30)
+        print('========== Testing ==========')
+        print('='*30)
+
+        self.model.eval()
+
+        input_str = ''.join(['<'+k+'> '+v+' </'+k+'>\n' for k, v in table.items()])
+        input_ids = self.tokenize(input_str).squeeze(0)
+        print('input_ids', input_ids.size())
+        outputs = self._generate_one_step(input_ids.unsqueeze(0))
+        print('outputs', outputs.size())
+
+        l = self.tokenizer.decode(outputs.squeeze(0).long().tolist())
+        l = l.strip('\n').strip().replace('<pad>', '').replace('</s>', '').strip().replace(' ', '\n')
+        l = converter.convert(l)
+        return l
 
 
